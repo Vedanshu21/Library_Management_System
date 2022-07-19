69fd62b08dafc72178fb9f6130fc4d0cb7daea7a
unknown <vedanshudewangan8@gmail.com>
2022-07-19 21:14:57 +0530

    Added files

---
+++ librarian/dbcon.php
@@ -0,0 +1,3 @@
+<?php
+mysql_select_db('eb_lms',mysql_connect('localhost','admin','admin'))or die(mysql_error());
+?>
---
+++ sidebar.php
@@ -0,0 +1,26 @@
+			<div class="life-side-bar">
+			<div class="hero-container">                  
+			
+					
+			</div>
+			
+			
+		
+					
+					
+							<ul class="nav nav-tabs nav-stacked">
+						<li class="">
+						<a   href="#">&nbsp;    Contact US</a>
+						</li>
+					</ul>
+<strong>Address</strong>
+<p>IIIT, Naya Raipur, Chhattisgarh, India </p>
+<p>Telephone No. :</p>
+<p>0000-0000000</p>
+
+
+
+			
+			</div>
+			
+	
